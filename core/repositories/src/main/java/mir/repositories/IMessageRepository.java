6274084dce1c4b4d8a90949eb823e41b80628870
SilentRemix <dshkhamraev@edu.hse.ru>
2021-03-01 11:52:00 +0300

    Repository module updated, but not finished

--- core/repositories/src/main/java/mir/repositories/IMessageRepository.java
+++ core/repositories/src/main/java/mir/repositories/IMessageRepository.java
@@ -4,7 +4,77 @@ import mir.models.ParsedMessage;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDateTime;
+import java.util.List;
+
 @Repository
 public interface IMessageRepository extends JpaRepository<ParsedMessage, Integer> {
 
+    // TODO: 3/1/2021 Need to write SQL queries with annotation @Query
+    /**
+     * Returns all messages with given MTI
+     * @param mti MTI
+     * @return list of messages
+     */
+    List<ParsedMessage> getByMti(String mti);
+
+    /**
+     * Returns a message with given transaction number
+     * @param transactionNumber Transaction number
+     * @return message
+     */
+    ParsedMessage getByTransactionNumber(String transactionNumber);
+
+    /**
+     * Returns list of messages in date range [start, end]
+     * @param start Start date
+     * @param end End date
+     * @return list of messages
+     */
+    List<ParsedMessage> getInDateRange(LocalDateTime start, LocalDateTime end);
+
+    /**
+     * Returns all messages with given HEX
+     * @param hex HEX
+     * @return list of messages
+     */
+    List<ParsedMessage> getByHex(String hex);
+
+    /**
+     * Returns all messages with given value of "edited"
+     * @param edited indicates if messages edited
+     * @return list of messages
+     */
+    List<ParsedMessage> getByEdited(boolean edited);
+
+    /**
+     * Deletes all messages with given MTI
+     * @param mti MTI
+     */
+    void deleteByMti(String mti);
+
+    /**
+     * Deletes one message with given transaction number
+     * @param transactionNumber Transaction number
+     */
+    void deleteByTransactionNumber(String transactionNumber);
+
+    /**
+     * Deletes all messages in given dateTime range
+     * @param start Start date
+     * @param end End date
+     */
+    void deleteInDateRange(LocalDateTime start, LocalDateTime end);
+
+    /**
+     * Deletes all messages with given HEX
+     * @param hex HEX
+     */
+    void deleteByHex(String hex);
+
+    /**
+     * Deletes all messages with given value of "edited"
+     * @param edited indicates if messages edited
+     */
+    void deleteByEdited(boolean edited);
 }
