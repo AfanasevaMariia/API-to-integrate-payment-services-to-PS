4bac185d093e9b7db2a59446a16e00452f1c1486
SilentRemix <dshkhamraev@edu.hse.ru>
2021-03-06 16:38:35 +0300

    Swagger connected

--- core/controllers/src/main/java/mir/controllers/MessageController.java
+++ core/controllers/src/main/java/mir/controllers/MessageController.java
@@ -1,5 +1,7 @@
 package mir.controllers;
 
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import mir.models.ParsedMessage;
 import mir.services.IMessageService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,7 +20,11 @@ public class MessageController {
         this.service = service;
     }
 
-    @GetMapping
+    @GetMapping(path = "/welcome")
+    @ApiResponses(value = {
+            @ApiResponse(code = 200, message = "Client successfully saved"),
+            @ApiResponse(code = 400, message = "The user already exists")
+    })
     public String welcome(){
         return "Welcome to Mir-Core";
     }
@@ -28,25 +34,25 @@ public class MessageController {
         return service.getMessages();
     }
 
-    // TODO: 3/6/2021 Will be removed
-    @Deprecated
-    @PostMapping
-    public void addMessage(@RequestBody ParsedMessage parsedMessage) {
-        service.addMessage(parsedMessage);
-    }
-
-    // TODO: 3/6/2021 Will be removed
-    @Deprecated
-    @DeleteMapping(path = "{id}")
-    public void deleteMessage(@PathVariable("id") Integer id) {
-        service.deleteMessageById(id);
-    }
-
-    // TODO: 3/6/2021 Will be removed
-    @Deprecated
-    @PutMapping(path = "{id}")
-    public void updateMessage(@PathVariable("id") Integer id,
-                              @RequestBody ParsedMessage parsedMessage) {
-        service.updateMessage(id, parsedMessage);
-    }
+//    // TODO: 3/6/2021 Will be removed
+//    @Deprecated
+//    @PostMapping
+//    public void addMessage(@RequestBody ParsedMessage parsedMessage) {
+//        service.addMessage(parsedMessage);
+//    }
+//
+//    // TODO: 3/6/2021 Will be removed
+//    @Deprecated
+//    @DeleteMapping(path = "{id}")
+//    public void deleteMessage(@PathVariable("id") Integer id) {
+//        service.deleteMessageById(id);
+//    }
+//
+//    // TODO: 3/6/2021 Will be removed
+//    @Deprecated
+//    @PutMapping(path = "{id}")
+//    public void updateMessage(@PathVariable("id") Integer id,
+//                              @RequestBody ParsedMessage parsedMessage) {
+//        service.updateMessage(id, parsedMessage);
+//    }
 }
--- core/routing/src/main/java/mir/routing/Application.java
+++ core/routing/src/main/java/mir/routing/Application.java
@@ -2,12 +2,22 @@ package mir.routing;
 
 //import mir.routing.exception.UnsupportedMessageTypeIDException;
 
+import mir.controllers.MessageController;
 import mir.routing.constants.Constants;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.RestController;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 import java.io.*;
 import java.net.ServerSocket;
@@ -17,6 +27,7 @@ import java.net.Socket;
 @EntityScan(basePackages = "mir.models")
 @ComponentScan(basePackages = "mir")
 @EnableJpaRepositories(basePackages = "mir.repositories")
+@EnableSwagger2
 public class Application {
 //    private static final int MTI_LENGTH = 4;
 
@@ -55,5 +66,22 @@ public class Application {
         // run();
 
         System.out.println(sendMessage("asdasd)"));
+    }
+
+    @Bean
+    Docket Docs(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+    @Bean
+    public ApiInfo apiInfo() {
+        final ApiInfoBuilder builder = new ApiInfoBuilder();
+        builder.title("Mir-Core").version("0.0.1-demo").license("(C) Copyright 2020")
+                .description("List of all the APIs of Mir-Core through Swagger UI");
+        return builder.build();
     }
 }
