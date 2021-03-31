9935dbbe917dfae27906eabcec73b1aa50d93160
SilentRemix <dshkhamraev@edu.hse.ru>
2021-03-31 22:51:51 +0300

    Database URL added

--- core/routing/src/main/java/mir/routing/emulator/Issuer.java
+++ core/routing/src/main/java/mir/routing/emulator/Issuer.java
@@ -9,12 +9,14 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.*;
 
 @RestController
+@RequestMapping("/main")
 public class Issuer {
 
     private final IMessageService service;
